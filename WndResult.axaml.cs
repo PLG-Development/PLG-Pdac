d33468a7cda90c3a30e851c9c6b6b87e3a49719c
Elias <elias.fierke@outlook.com>
2024-12-28 11:42:58 +0100

    [feat:] initialized result window

---
+++ WndResult.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace PLG-Pdac;
+
+public partial class WndResult : Window
+{
+    public WndResult()
+    {
+        InitializeComponent();
+    }
+}
