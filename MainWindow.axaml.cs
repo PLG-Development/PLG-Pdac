569a06ada1d89eea2901c195dc07833e9521f2ad
Elias <elias.fierke@outlook.com>
2024-12-28 11:41:31 +0100

    [feat:] image source implemented

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,6 +1,8 @@
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia;
+using Avalonia.Media;
+using Avalonia.Media.Imaging;
 
 namespace PLG_Pdac;
 
@@ -9,6 +11,7 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        ImageV.Source = new Bitmap("resources/LOGO_white.png");
     }
 
     public void Upload_Click(object sender, RoutedEventArgs e){
