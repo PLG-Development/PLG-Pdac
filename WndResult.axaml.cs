5381bd0f63fe530a203e3d20a79b552dfcf867f3
Elias <elias.fierke@outlook.com>
2024-12-28 11:48:03 +0100

    [fix:] fixed namespace issues

--- WndResult.axaml.cs
+++ WndResult.axaml.cs
@@ -1,13 +1,20 @@
-using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
+using Avalonia.Interactivity;
+using Avalonia;
+using Avalonia.Media;
+using Avalonia.Media.Imaging;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
-namespace PLG-Pdac;
+namespace PLG_Pdac;
 
 public partial class WndResult : Window
 {
-    public WndResult()
+    public string filePath;
+    public WndResult(string path)
     {
         InitializeComponent();
+
+        filePath = path;
     }
 }
